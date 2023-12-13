de1b32b11bbf510280ff1ae66fb182e58b669702
tesfustar <tesfu9503@gmail.com>
2023-12-13 16:12:37 +0300

    basic dashboard layout id done

--- src/config/site.config.tsx
+++ src/config/site.config.tsx
@@ -23,10 +23,10 @@ export const metaObject = (
   description: string = siteConfig.description
 ): Metadata => {
   return {
-    title: title ? `${title} - Lively Expert Dashboard` : siteConfig.title,
+    title: title ? `${title} - Lively` : siteConfig.title,
     description,
     openGraph: openGraph ?? {
-      title: title ? `${title} - Lively  Expert Dashboard` : title,
+      title: title ? `${title} - Lively` : title,
       description,
       url: "https://lively-expert.vercel.app",
       siteName: "Lively - Expert Dashboard", // https://developers.google.com/search/docs/appearance/site-names
