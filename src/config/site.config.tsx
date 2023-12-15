de964b87efb6ac8cbc1e7e85672e53f557f2499c
dan mulugeta <mulugetadawit55@gmail.com>
2023-12-15 16:08:51 +0000

    Updated site.config.tsx

--- src/config/site.config.tsx
+++ src/config/site.config.tsx
@@ -28,7 +28,7 @@ export const metaObject = (
     openGraph: openGraph ?? {
       title: title ? `${title} - Lively` : title,
       description,
-      url: "https://lively-expert.vercel.app",
+      url: "https://livelyexper.azurewebsites.net",
       siteName: "Lively - Expert Dashboard", // https://developers.google.com/search/docs/appearance/site-names
       images: {
         url: "https://lively-landing-o64h8i5v7-unravel.vercel.app/thumbnail.png",
