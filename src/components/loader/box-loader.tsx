11bb679ef7fc525109b68c9b419c5611978855ca
kbleul <kbleulseged@gmail.com>
2024-02-07 23:14:32 -0500

    DONE: SO banner

--- src/components/loader/box-loader.tsx
+++ src/components/loader/box-loader.tsx
@@ -3,9 +3,9 @@ import { BarLoader } from "react-spinners";
 
 const BoxLoader = () => {
   return (
-    <div className="w-full px-4 md:px-10 py-4 md:py-8 rounded-xl bg-yellow-100 ">
+    <div className="w-full mb-6 px-4 md:px-10 py-4 md:py-8 rounded-xl bg-yellow-100 flex justify-center items-center">
       <BarLoader
-        color={"#00BA63"}
+        color={"#000"}
         loading={true}
         aria-label="Loading Spinner"
         data-testid="loader"
