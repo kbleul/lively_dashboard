5bf152c3399516ffdb8881c97949751c1d757c0b
kbleul <kbleulseged@gmail.com>
2024-02-27 12:07:16 +0300

    expert appointment new logic

--- src/app/shared/expert/availability/expert-availability.tsx
+++ src/app/shared/expert/availability/expert-availability.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 "use client";
 import { Formik, Form } from "formik";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import FormGroup, { FormBlockWrapper } from "@/components/form-group";
 import FormikInput from "@/components/ui/form/input";
 import { toast } from "sonner";
@@ -15,11 +15,24 @@ import { useGetHeaders } from "@/hooks/use-get-headers";
 import { workCustomDays } from "@/constants/form-constants";
 import { appendDefaultSecond } from "@/utils/append-second";
 import { useQueryClient } from "@tanstack/react-query";
+import { DaysAxis } from "../appointments/components/data";
+import { Input, Title } from "rizzui";
+import Spinner from "@/components/ui/spinner";
+import { GoPlusCircle } from "react-icons/go";
+
 const ExpertAvailability = ({ className }: { className?: string }) => {
   const queryClient = useQueryClient();
   const postMutation = useDynamicMutation();
   const headers = useGetHeaders({ type: "Json" });
-  const [customDaysChecked, setCustomDaysChecked] = React.useState(
+
+  const [selectedDayofWeek, setSelectedDayofWeek] = useState(DaysAxis[0]);
+  const [startingTime, setStartingTime] = useState<string | null>(null);
+  const [endingTime, setEndingTime] = useState<string | null>(null);
+  const [addTimes, setAddedTimes] = useState<
+    { startingTime: string; endingTime: string }[]
+  >([]);
+
+  const [customDaysChecked, setCustomDaysChecked] = useState(
     Array(7).fill(false)
   );
   const schema = Yup.object().shape({
@@ -38,22 +51,6 @@ const ExpertAvailability = ({ className }: { className?: string }) => {
     headers
   );
 
-  useEffect(() => {
-    const standardWeekDayIndices: Record<string, number> = {};
-    expertAvialbility?.data?.data
-      ?.map((data: { day_of_week: string }) => data.day_of_week)
-      .forEach((day: string, index: number) => {
-        standardWeekDayIndices[day] = index;
-      });
-    const values = Object.values(standardWeekDayIndices);
-    const newArray = [...customDaysChecked];
-    values.forEach((index) => {
-      if (index >= 0 && index < newArray.length) {
-        newArray[index] = true;
-      }
-    });
-    setCustomDaysChecked(newArray);
-  }, [expertAvialbility.status]);
   const updateAvailability = async (values: any) => {
     try {
       await postMutation.mutateAsync({
@@ -85,69 +82,130 @@ const ExpertAvailability = ({ className }: { className?: string }) => {
     }
   };
   const initialValues = {
-    openingHours:
-      expertAvialbility?.data?.data?.length > 0
-        ? expertAvialbility?.data?.data
-        : workCustomDays,
+    openingHours: workCustomDays[0],
+    from: workCustomDays[0].from,
+    to: workCustomDays[0].to,
   };
+  console.log(expertAvialbility?.data?.data);
+  const expertAvialbleTime = expertAvialbility?.data?.data;
+
+  console.log(expertAvialbleTime);
+
   return (
     <Formik
       initialValues={initialValues}
       validationSchema={schema}
       onSubmit={updateAvailability}
     >
-      {({ errors, setFieldValue, values }) => (
+      {({ values }) => (
         <Form className="flex flex-grow flex-col @container [&_label]:font-medium">
           <div className="grid grid-cols-1 gap-8 divide-y divide-dashed divide-gray-200 @2xl:gap-10 @3xl:gap-12">
             <FormBlockWrapper
               title={"Availability Time."}
-              description={"Edit Availability Time from here"}
+              description={"Add Availability Time from here"}
               className="py-7 @2xl:pt-9 @3xl:pt-11"
             >
               {expertAvialbility.isFetched && !expertAvialbility.isFetching ? (
                 <div className="col-span-2">
-                  {values.openingHours.map((_: any, index: number) => (
-                    <div className="flex items-end  gap-2 w-full " key={index}>
-                      <Checkbox
-                        checked={customDaysChecked[index]}
-                        variant="flat"
-                        color="primary"
-                        className="font-medium"
-                        onChange={(e) => {
-                          const isChecked = e.target.checked;
-                          setCustomDaysChecked((prevChecked) => {
-                            const newChecked = [...prevChecked];
-                            newChecked[index] = isChecked;
-                            return newChecked;
-                          });
-                        }}
-                      />
+                  <section className="flex justify-start gap-4  flex-wrap mb-8">
+                    {DaysAxis.map((day, index) => (
+                      <button
+                        onClick={() =>
+                          day !== selectedDayofWeek && setSelectedDayofWeek(day)
+                        }
+                        key={"appdayskey" + day + index}
+                        type="button"
+                        className={
+                          selectedDayofWeek === day
+                            ? "border rounded-full px-4 py-2 font-semibold text-white bg-gradient-to-r from-[#008579] to-[#00BA63]"
+                            : "border rounded-full px-4 py-2 font-medium"
+                        }
+                      >
+                        {day}
+                      </button>
+                    ))}
+                  </section>
 
-                      <FormikInput
-                        name={`openingHours[${index}].day`}
-                        label="Day"
-                        disabled
-                        color="primary"
+                  <section className="flex justify-start gap-4  flex-wrap mb-8">
+                    {expertAvialbleTime
+                      .filter(
+                        (avTime: any) =>
+                          avTime.day_of_week === selectedDayofWeek
+                      )
+                      .map((day: any) => (
+                        <button
+                          key={"addedAppkey" + day.id}
+                          type="button"
+                          className="border bg-gray-100 rounded-md px-4 py-2"
+                        >
+                          {day.opening_time + " - " + day.closing_time}
+                        </button>
+                      ))}
+
+                    {addTimes.map((time, index: number) => (
+                      <button
+                        key={
+                          "addedtimeAppkey" +
+                          time.startingTime +
+                          time.endingTime
+                        }
+                        type="button"
+                        className="border bg-white rounded-md px-4 py-2"
+                      >
+                        {time.startingTime + " - " + time.endingTime}
+                      </button>
+                    ))}
+                  </section>
+
+                  <section className="shadow-md p-4 mt-4 border-t rounded-md">
+                    <Title as="h4" className="mt-4 mb-8 font-normal border-b">
+                      Add a new appointment slot for {selectedDayofWeek}
+                    </Title>
+
+                    <div className="flex flex-col md:flex-row gap-4">
+                      <Input
+                        autoComplete="off"
+                        // @ts-ignore
+                        type="time"
+                        label={"Starting Time"}
+                        name={"name"}
+                        onChange={(e) =>
+                          setStartingTime(e.target.value + ":00")
+                        }
                       />
-                      <FormikInput
-                        name={`openingHours[${index}].from`}
-                        label="Opening Time"
-                        disabled={!customDaysChecked[index]}
-                        type="time24"
-                        color="primary"
-                      />
-                      <FormikInput
-                        name={`openingHours[${index}].to`}
-                        label="Closing Time"
-                        type="time24"
-                        color="primary"
-                        disabled={!customDaysChecked[index]}
+                      <Input
+                        autoComplete="off"
+                        // @ts-ignore
+                        type="time"
+                        label={"Ending Time"}
+                        name={"name"}
+                        onChange={(e) => setEndingTime(e.target.value + ":00")}
                       />
                     </div>
-                  ))}
+
+                    <button
+                      onClick={() => {
+                        if (startingTime && endingTime) {
+                          setAddedTimes((prev) => [
+                            ...prev,
+                            { startingTime, endingTime },
+                          ]);
+
+                          setStartingTime(null);
+                          setEndingTime(null);
+                        }
+                      }}
+                    >
+                      <GoPlusCircle />
+                    </button>
+
+                    <button className=""></button>
+                  </section>
                 </div>
               ) : (
-                <>Loading...</>
+                <div className="grid h-full min-h-[128px] flex-grow place-content-center items-center justify-center">
+                  <Spinner size="lg" />
+                </div>
               )}
             </FormBlockWrapper>
           </div>
