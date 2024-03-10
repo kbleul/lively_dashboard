e9e4d92030ae456adb6f746ce1bc37e2a719e3ce
kbleul <kbleulseged@gmail.com>
2024-03-10 15:26:03 +0300

    add localization to apis counsellor

--- src/validations/assessment.ts
+++ src/validations/assessment.ts
@@ -17,35 +17,59 @@ export type CategoriesType = {
 };
 
 export const mindfulnessCategoriesSchema = Yup.object().shape({
-  nameEnglish: Yup.string().required("Category name is required"),
-  descriptionEnglish: Yup.string().required("Category description is required"),
+  nameEnglish: Yup.string().required("English category name is required"),
+  nameAmharic: Yup.string().required("Amharic Category name is required"),
+  descriptionEnglish: Yup.string().required(
+    "English category description is required"
+  ),
+  descriptionAmharic: Yup.string().required(
+    "Amharic category description is required"
+  ),
   image: Yup.mixed().required("Cover image is required"),
 });
 
 export type mindfulnessCategoriesType = {
+  nameAmharic: string;
   nameEnglish: string;
   descriptionEnglish: string;
+  descriptionAmharic: string;
   image: string;
 };
 
 export const mindfulnessExerciseSchema = Yup.object().shape({
-  titleAmharic: Yup.string().required("Exercise title is required"),
-  masterAmharic: Yup.string().required("Narrator is required"),
-  duration: Yup.string().required("Narrator is required"),
-  audioAmharic: Yup.mixed().required("Audio is required"),
+  titleAmharic: Yup.string().required("Amharic exercise title is required"),
+  titleEnglish: Yup.string().required("English exercise title is required"),
+
+  masterAmharic: Yup.string().required("Narrator amharic name is required"),
+  masterEnglish: Yup.string().required("Narrator english name is required"),
+
+  durationAmharic: Yup.string().required("Amharic audio duration is required"),
+  durationEnglish: Yup.string().required("English audio duration is required"),
+
+  audioAmharic: Yup.mixed().required("Amharic Audio is required"),
+  audioEnglish: Yup.mixed().required("English Audio is required"),
 });
 
 export const editMindfulnessExerciseSchema = Yup.object().shape({
-  titleAmharic: Yup.string().required("Exercise title is required"),
-  masterAmharic: Yup.string().required("Narrator is required"),
-  duration: Yup.string().required("Narrator is required"),
+  titleAmharic: Yup.string().required("Amharic exercise title is required"),
+  titleEnglish: Yup.string().required("English exercise title is required"),
+
+  masterAmharic: Yup.string().required("Narrator amharic name is required"),
+  masterEnglish: Yup.string().required("Narrator english name is required"),
+
+  durationAmharic: Yup.string().required("Amharic audio duration is required"),
+  durationEnglish: Yup.string().required("English audio duration is required"),
 });
 
 export type mindfulnessExerciseType = {
+  titleEnglish: string;
   titleAmharic: string;
   masterAmharic: string;
-  duration: string;
+  masterEnglish: string;
+  durationAmharic: string;
+  durationEnglish: string;
   audioAmharic: string;
+  audioEnglish: string;
 };
 
 export const assessmentQuestionschema = Yup.object().shape({
